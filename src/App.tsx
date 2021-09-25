9e0529e9c95300983d729ba751e24747f2b334e4
RafaelGoulartB <rafagoulartb@gmail.com>
2021-09-25 12:59:31 -0300

    🎨 styles: Creating global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import styled from 'styled-components'
+import { GlobalStyle } from './styles/global';
 
 const App: React.FC = () => {
   return ( 
     <div>
-      <p>test</p>
+      <GlobalStyle />
+      <h1>Hello World</h1>
     </div>
   );
 }
