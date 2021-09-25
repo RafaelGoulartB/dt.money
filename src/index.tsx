0b418d556bdc4fce320211ed4cc4faf0a5c78aec
RafaelGoulartB <rafagoulartb@gmail.com>
2021-09-25 17:06:38 -0300

    🔨 build: Install and config miragejs for mock data

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './App';
+import { createServer } from 'miragejs'
+
+createServer({
+  routes() {
+    this.namespace = 'api'
+    this.get('/transactions', () => {
+      return [
+        {
+          id: 1,
+          title: 'Transaction 1',
+          amount: 400,
+          type: 'deposit',
+          category: 'Food',
+          createdAt: new Date(),
+        }
+      ]
+    })
+  }
+})
 
 ReactDOM.render(
   <React.StrictMode>
